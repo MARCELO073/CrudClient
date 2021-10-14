bffd46f82f08fb5c2668e33b65598bcd66d8dbf2
MARCELO073 <marcelonogueira072@gmail.com>
2021-10-14 04:39:31 -0300

    exeception handling

--- backend/src/main/java/com/MARCELO073/CrudClient/services/ClientService.java
+++ backend/src/main/java/com/MARCELO073/CrudClient/services/ClientService.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.MARCELO073.CrudClient.dto.ClientDTO;
 import com.MARCELO073.CrudClient.entities.Client;
+import com.MARCELO073.CrudClient.exeception.ResourceNotFoundException;
 import com.MARCELO073.CrudClient.repositories.ClientRepository;
 
 @Service
@@ -19,18 +20,18 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	
+	// busca todos os objetos
     @Transactional(readOnly = true) 
 	public List<ClientDTO> findAll() {
     	List<Client> list = repository.findAll();
     	return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
  
     	}
-    
+    // busca obijeto por id, caso não encontre passa excessão
     @Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
     	Optional<Client> obj = repository.findById(id);
-    	Client entity = obj.get();
+    	Client entity = obj.orElseThrow(()-> new ResourceNotFoundException("Entity not found"));
     	
 	
 		return new ClientDTO(entity);
